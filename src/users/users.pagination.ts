5321c1d7b516c96e34755aba2112e94b666beaf7
max-ghz <userbarabiasz95@gmail.com>
2026-08-28 23:11:17 +0200

    feat: add endpoint for 'get all users'

---
+++ src/users/users.pagination.ts
@@ -0,0 +1,20 @@
+import { PaginateConfig, PaginationType } from 'nestjs-paginate';
+import { DEFAULT_LIMIT, DEFAULT_MAX_LIMIT } from '@api/pagination.constants';
+import { UserEntity } from './user.entity';
+
+export const USERS_PAGINATION_CONFIG: PaginateConfig<UserEntity> = {
+  sortableColumns: [
+    'id',
+    'username',
+    'uniqueCaps',
+    'hardest',
+    'gold',
+    'mapsCreated',
+  ],
+  searchableColumns: ['username'],
+  defaultSortBy: [['id', 'ASC']],
+  filterableColumns: {},
+  maxLimit: DEFAULT_MAX_LIMIT,
+  defaultLimit: DEFAULT_LIMIT,
+  paginationType: PaginationType.LIMIT_AND_OFFSET,
+};
---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { paginate, PaginateQuery, Paginated } from 'nestjs-paginate';
+import { UserEntity } from './user.entity';
+import { USERS_PAGINATION_CONFIG } from './users.pagination';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>,
+  ) {}
+
+  findAll(query: PaginateQuery): Promise<Paginated<UserEntity>> {
+    return paginate(query, this.usersRepository, USERS_PAGINATION_CONFIG);
+  }
+}
